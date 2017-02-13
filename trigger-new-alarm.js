37f4c7cb81d94008dcb6181315d787d74a94474d
@supercobra <daniel@metadot.com>
2017-02-13 15:06:08 -0600

    Create trigger-new-alarm.js

---
+++ trigger-new-alarm.js
@@ -0,0 +1,18 @@
+/** Example only. Does not work yet */
+
+"use strict";
+var fetch = require('node-fetch')
+var url = 'http://www.montastic.com/feeds/quick_status?key=' + dddddddddddddddddddkey;
+var count = 0;
+fetch(url)
+    .then(function (res) {
+        return res.text();
+    }).then(function (data) {
+        console.log('data from fetch: ' + data);
+        if (data > 0) {
+            console.log('Montastic alarms: ' + data);
+        } else {
+            console.log('OK');
+        }
+        console.log(data);
+    });
